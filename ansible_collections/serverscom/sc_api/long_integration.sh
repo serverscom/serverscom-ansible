1e8c65a5c0772a0ecb8a7d7cefd7f57a5762e082
George Shuklin <george.shuklin@gmail.com>
2020-09-01 16:54:46 +0300

    Integration test runner

---
+++ ansible_collections/serverscom/sc_api/long_integration.sh
@@ -0,0 +1,1 @@
+ansible-test integration --python=3.8 sc_dedicated_server_reinstall_long
---
+++ ansible_collections/serverscom/sc_api/medium_integration.sh
@@ -0,0 +1,1 @@
+ansible-test integration --python=3.8 sc_ssh_key sc_cloud_computing_instance sc_cloud_computing_instance_reinstall
