c15a6241c4868854576332810f39a1eaa9cc1d81
Aleksandr Chudinov <silentirk@me.com>
2025-11-04 15:03:09 +0200

    ensure every parameter needed for creation of volume is provided

--- ansible_collections/serverscom/sc_api/plugins/module_utils/modules.py
+++ ansible_collections/serverscom/sc_api/plugins/module_utils/modules.py
@@ -2297,6 +2297,9 @@ class scRBSVolumeCreateUpdateDelete:
                 self.volume_id = existing_volume["id"]
                 upd = self.update_volume()
                 return upd
+        else:
+            if not(self.location_id and self.flavor_id and self.size):
+                raise ModuleError(f"RBS volume with name '{self.name}' does not exist. To create it, location_id (or location_code), flavor_id (or flavor_name) and size must be provided.")
         if not self.checkmode:
             response = self.api.create_rbs_volume(
                 name=self.name,
